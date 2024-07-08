b1b7c66cf0768d187b47f058fb2f09dbec61146b
Arbaz <arbazshaikh1507@gmail.com>
2024-07-08 15:00:55 +0530

    Partially Completed the blog page and made several components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { Routes , Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Blog from './pages/Blog';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/about' element={<About />}></Route>
+      <Route path='/blogs' element={<Blog />}></Route>
+
     </Routes>
   );
 }
